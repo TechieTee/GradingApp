081b7a058a4e1a03558ec4766bbb1cf71825e3bf
TechieTee <mailtomodupe@gmail.com>
2020-07-11 00:55:00 -0700

    changed stuffs

--- js/script.js
+++ js/script.js
@@ -1,44 +1,116 @@
+console.log("yayyyy!");
+
+var sub = document.querySelector("#subject");
+var sco = document.querySelector("#score");
+var gra = document.querySelector("#grade");
+var gen = document.querySelector("#generate");
+var list = document.querySelector("#list");
+
+// var div1 = document.querySelector("#div1");
+// var div2 = document.querySelector("#div2");
+// var div3 = document.querySelector("#div3");
+
+// var additem = document.querySelector("#additem");
 
 
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
-}
+var clear= document.querySelector("#clear");
+var calc = document.querySelector("#calcCGPA");
+var row = document.querySelector("#NewRow");
+var warn = document.querySelector("#Warning");
+var cgpa = document.querySelector("#cgpa");
+// let GradeArray = [];
 
-var list = document.querySelector("#list")
-var additem = document.querySelector(".addItem");
-additem.addEventListener('click', (e) => {
-  e.preventDefault();
-  var li = document.createElement('li');
-  var newitem = document.querySelector("#newItem");
-   li.innerHTML = newitem.value;
-   list.appendChild(li);
-   newitem.value="";
 
-   var span = document.createElement("span");
-   var x = document.createElement("IMG");
-   x.setAttribute("src", "images/delete2.png");
-   x.setAttribute("width", "20");
-   x.setAttribute("height", "20");
-   x.setAttribute("alt", "delete-item");
-   //var txt= document.createTextNode("m");
-   span.className = "close"; 
-   span.appendChild(x);
-   li.appendChild(span);
-     for (let i = 0; i < close.length; i++) {
-      
-       close[i].onclick = function() {
-         var div = this.parentElement;
-         div.style.display = "none";
-         
-       }
-      
-       
-     }
-     
-}
+gen.addEventListener('click', (e) => {
+    e.preventDefault();
+    
+ if (sub.value == "" || sco.value == "") {
+    warn.innerHTML = "you must input a subject and a score";
+   cgpa.innerHTML = "";
+   
+ } else {
+        if (sco.value <= 35) {gra.value = "F"}
+   else if (sco.value <= 40 && sco.value > 35) {gra.value = "E"}
+   else if (sco.value <= 50 && sco.value > 40) {gra.value = "D"}
+   else if (sco.value <= 60 && sco.value > 50) {gra.value = "C"}
+   else if (sco.value <= 70 && sco.value > 60) {gra.value = "B"}
+   else if (sco.value <= 100 && sco.value > 70) {gra.value = "A"}
+      else{ warn.innerHTML = "invalid input, Grade must be a value from one to one hundred";
+           cgpa.innerHTML = "";
+    
+     return false;
+   }
+   oya();
+      warn.innerHTML = "";
+   cgpa.innerHTML = "";
+
+      //  sub.value = "";
+      //  sco.value = "";
+      // gra.value = "";
+   
+ }  } 
+ 
 )
+
+function oya() {
+   // additem.addEventListener('click', (e) => {
+   //    e.preventDefault();
+     
+   
+   
+      var li = document.createElement('li');
+     
+       li.innerHTML = sub.value;
+       list.appendChild(li);
+       sub.value="";
+      
+    
+       var span = document.createElement("span");
+      span.className = "stylescore"; 
+     
+       span.innerHTML = sco.value;
+       
+      
+       li.appendChild(span);
+      
+       sco.value="";
+   
+   
+       var spantwo = document.createElement("span");
+       spantwo.className = "stylescoretwo"; 
+     
+        spantwo.innerHTML = gra.value;
+        
+       
+        li.appendChild(spantwo);
+       
+       
+         // for (let i = 0; i < close.length; i++) {
+          
+         //   close[i].onclick = function() {
+         //     var div = this.parentElement;
+         //     div.style.display = "none";
+         //      }
+         // }   
+   //  }
+   //  )  
+}
+
+clear.addEventListener('click', (e) => {
+   e.preventDefault();
+  
+sub.value  = "";
+sco.value  = "";
+gra.value  = "";
+list.innerHTML = "";
+warn.innerHTML = "";
+  cgpa.innerHTML = "";
+})
+
+calc.addEventListener('click', (e) => {
+   e.preventDefault();
+   cgpa.innerHTML = "Coming soon!";
+   warn.innerHTML = "";
+})
+
+
